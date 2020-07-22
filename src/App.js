83f6980cc509214a8b6d7f91adf6b0ef474fbe00
Athinarayanan <athifrank@gmail.com>
2020-07-22 20:05:11 +0530

    commit for cards

--- src/App.js
+++ src/App.js
@@ -2,25 +2,86 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+class Card{
+  constructor(suit,value){
+    this.suit=suit;
+    this.value=value;
+  }
+}
+
+const deck=[]
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+       deck:[]
+    }
+  }
+
+  createDeck=()=>{
+    for (let suit in suits) {
+      for (let value in values) {
+        deck.push(new Card(suits[suit],values[value]))
+        this.setState({deck})
+      }
+    }
+  }
+
+  shuffle=()=>{
+    let deck = [...this.state.deck];
+    let m = deck.length, i;
+    while(m){
+      i = Math.floor(Math.random() * m--);
+      [deck[m], deck[i]] = [deck[i], deck[m]];
+    }
+    this.setState({deck:deck})
+    //return deck;
+  }
+
+  componentDidMount(){
+    this.createDeck()
+    //console.log('deck ',this.shuffle())
+  }
+
+  deal=()=>{
+    this.setState({deck:deck.pop()})
+  }
+
+  render(){
+   const data=this.state.deck.length===0 ? [] : this.state.deck
+
+   console.log('dta' ,data)
+    return (
+      <div >
+        <button onClick={()=>this.shuffle()}>shuffle</button>
+        <button onClick={()=>this.createDeck()}>reset</button>
+        {
+          this.state.deck.length===0? '' :
+          <div className="divCard">
+            {
+              this.state.deck.map((res,inx)=>{
+                return(
+                  <div className="card">
+                    {
+                      res.suit 
+                    }
+                    {
+                      res.value
+                    }
+                  </div>
+                )
+              })
+            }
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
